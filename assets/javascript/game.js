a8404d63707ed800b6a191498393f8eb4b036c65
jstudenski <jstudenski@gmail.com>
2017-12-06 21:55:58 -0600

    minor changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,7 @@
 window.onload = function() {
 
+
+
 // variables
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var drinkNames = ['water water']; // array of possible words
